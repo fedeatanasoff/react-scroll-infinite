fc561376e38572191a396d1f76072358a9affc8a
fedeatanasoff <fede.atanasoff@gmail.com>
2019-02-26 01:44:24 -0300

    corrigiendo el error de la key en la iteracion

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -4,21 +4,15 @@ export class Hero extends Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1 className="display-5">
-          Desafio Unsplash Scroll Infinito{" "}
-        </h1>
+        <h1 className="display-5">Desafio Unsplash Scroll Infinito </h1>
         <p className="lead">
-          Galeria de imagenes con scroll infinito usando
-          Node.js, React y Unsplash API.
+          Galeria de imagenes con scroll infinito usando Node.js, React y
+          Unsplash API.
         </p>
         <hr className="my-4" />
         <p>Este es un desafio de Scotch.io</p>
         <p className="lead">
-          <a
-            className="btn btn-primary btn-lg"
-            href="#"
-            role="button"
-          >
+          <a className="btn btn-primary btn-lg" href="#" role="button">
             Visitar pagina del desafio
           </a>
         </p>
--- client/src/components/image/Image.jsx
+++ client/src/components/image/Image.jsx
@@ -22,7 +22,9 @@ export class Image extends Component {
     this.setState({ start: this.state.start + count });
     axios
       .get(`api/photos?count=${count}&start=${start}`)
-      .then(res => this.setState({ images: res.data }));
+      .then(res =>
+        this.setState({ images: this.state.images.concat(res.data) })
+      );
   };
 
   render() {
@@ -34,14 +36,9 @@ export class Image extends Component {
         hasMore={true}
         loader={<h4> cargando...</h4>}
       >
-        {this.state.images.map(imagen => (
-          <div className="card">
-            <img
-              key={imagen.id}
-              src={imagen.urls.regular}
-              alt=""
-              width="100%"
-            />
+        {this.state.images.map((imagen, index) => (
+          <div className="card" key={index}>
+            <img src={imagen.urls.regular} alt="" width="100%" />
           </div>
         ))}
       </InfiniteScroll>
